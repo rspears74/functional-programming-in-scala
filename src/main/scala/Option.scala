f3d0d66826103cbba837eebe33519af4f52169df
Randall Spears <rspears690@gmail.com>
2022-11-26 19:53:43 -0700

    Delete target files

--- src/main/scala/Option.scala
+++ src/main/scala/Option.scala
@@ -1,10 +1,10 @@
-trait MyOption[+A] {
-  def map[B](f: A => B): MyOption[B] = this match {
+trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
     case None => None
     case Some(x) => Some(f(x))
   }
 
-  def flatMap[B](f: A => MyOption[B]): MyOption[B] = this match {
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
     case None => None
     case Some(x) => f(x)
   }
@@ -14,25 +14,44 @@ trait MyOption[+A] {
     case Some(x) => x
   }
 
-  def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = this match {
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
     case None => ob
     case _ => this
   }
 
-  def filter(f: A => Boolean): MyOption[A] = this match {
+  def filter(f: A => Boolean): Option[A] = this match {
     case Some(x) if f(x) => this
     case _ => None
   }
 }
 
-case class Some[+A](get: A) extends MyOption[A]
-case object None extends MyOption[Nothing]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
 
 object Option {
-  def mean(xs: Seq[Double]): MyOption[Double] =
+  def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): MyOption[Double] =
+  def variance(xs: Seq[Double]): Option[Double] =
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(aVal => b.map(bVal => f (aVal, bVal)))
+
+  def sequence[A](a: MyList[Option[A]]): Option[MyList[A]] = a match {
+    case Nil => Some(Nil)
+    case Cons(x, xs) => x.flatMap(xVal => sequence(xs).map(Cons(xVal, _)))
+  }
+
+  def traverse[A,B](a: MyList[A])(f: A => Option[B]): Option[MyList[B]] = a match {
+    case Nil => Some(Nil)
+    case Cons(x, xs) => map2(f(x), traverse(xs)(f))(Cons(_, _))
+  }
+
+  def traverse2[A,B](a: MyList[A])(f: A => Option[B]): Option[MyList[B]] =
+    MyList.foldRight[A, Option[MyList[B]]](a, Some(Nil))((h, t) => map2(f(h), t)(Cons(_, _)))
+
+  def sequence2[A](a: MyList[Option[A]]): Option[MyList[A]] =
+    traverse(a)(v => v)
 }
--- src/test/scala/OptionTests.scala
+++ src/test/scala/OptionTests.scala
@@ -1,3 +1,35 @@
-class OptionTests {
+import org.scalatest.funsuite.AnyFunSuite
+import Option._
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
+class OptionTests extends AnyFunSuite with Matchers {
+  test("variance") {
+    assertResult(Some(0.0))(variance(Seq(1, 1, 1, 1)))
+  }
+
+  test("map2") {
+    assertResult(Some(5))(map2(Some(2), Some(3))(_ + _))
+  }
+
+  test("sequence") {
+    sequence(MyList(Some(1), Some(2), Some(3))) shouldBe Some(MyList(1, 2, 3))
+  }
+
+  def div(num: Double, den: Double): Option[Double] = den match {
+    case 0 => None
+    case _ => Some(num / den)
+  }
+
+  test("traverse") {
+    traverse(MyList(4, 2, 1))(div(1, _)) shouldBe Some(MyList(0.25, 0.5, 1.0))
+  }
+
+  test("traverse2") {
+    traverse2(MyList(4, 2, 1))(div(1, _)) shouldBe Some(MyList(0.25, 0.5, 1.0))
+  }
+
+  test("sequence2") {
+    sequence2(MyList(Some(1), Some(2), Some(3))) shouldBe Some(MyList(1, 2, 3))
+  }
 }
